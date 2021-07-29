19fb8fcc60f8e55f3c1cad5c135835feb51eda07
sinbum <sinbum@kakao.com>
2021-07-29 15:07:17 +0900

    공백수정

--- src/main/java/index/controller/MainController.java
+++ src/main/java/index/controller/MainController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-
 public class MainController {
 	ModelAndView mv;
 	public MainController() {
