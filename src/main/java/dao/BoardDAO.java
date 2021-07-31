39c595bf7c1edb04812c37f8785c139fbcb1246a
[sinbum] <[sinbum@kakao.com]>
2021-07-31 18:10:41 +0900

    boardvo> boardvo로 변경 및 수정

--- src/main/java/dao/BoardDAO.java
+++ src/main/java/dao/BoardDAO.java
@@ -3,13 +3,13 @@ package dao;
 import java.util.List;
 
 import vo.BoardDetailVO;
-import vo.BoardsVO;
+import vo.BoardVO;
 import vo.TotalBoardVO;
 
 public interface BoardDAO {
 	public List <TotalBoardVO> getTotallist();
-	public List <BoardsVO> getlist();
-	public BoardsVO getboardvo(int num);
+	public List <BoardVO> getlist();
+	public BoardVO getboardvo(int num);
 	public BoardDetailVO getboarddetail(int boardnum);
 	public TotalBoardVO getTotalboard(String parameter);		
 	public int board_count_update();
