23486f422b747b0ae0aa332b342b20c91a2ddeb6
sinbum <sinbum@sinbum-B360M-DS3H>
2021-08-01 19:05:33 +0900

    마이바티스 회원가입기능 연동추가

--- src/main/java/config/mapper/MainMapper.java
+++ src/main/java/config/mapper/MainMapper.java
@@ -5,7 +5,7 @@ import org.apache.ibatis.annotations.Param;
 public interface MainMapper {
 	public int loginCheck(@Param("id") String id,@Param("password") String password);
 
-	public int signIn(String id, String password, String email);
+	public int signIn(@Param("id")String id, @Param("password") String password,@Param("email") String email);
 
 	public int makeChannel();
 }
