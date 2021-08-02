86ec0327d7d33bc02575d0f6aa9a770f0da5f089
sinbum <sinbum@kakao.com>
2021-08-02 17:34:05 +0900

    서비스impl 내 채널추가 기능

--- src/main/java/service/MainServiceImple.java
+++ src/main/java/service/MainServiceImple.java
@@ -1,10 +1,13 @@
 package service;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import config.mapper.MainMapper;
+import vo.BoardChannelVO;
 
 
 @Service
@@ -24,9 +27,30 @@ public class MainServiceImple implements MainService{
 	}
 
 	@Override
-	public int makeChannel() {
-		return 0;
+	public int makeChannel(
+			String channel_name, 
+			String channel_info, 
+			String channel_category,
+			String member_id_fk) {
+		return mainmapper.makeChannel(channel_name, channel_info, channel_category, member_id_fk);
 	}
+
+	public int makeBoardDo(			
+					String board_firstkeyword,
+					String board_secondkeyword,
+					String board_name,
+					String board_content,
+					int channel_num) {
+		return mainmapper.makeBoardDo(board_firstkeyword, board_secondkeyword, board_name, board_content, channel_num);
+			}
+	
+	@Override
+	public List<BoardChannelVO> getChannelList(String memberId){
+
+		return mainmapper.getChannelList(memberId);
+	}
+
+	
 
 
 	
