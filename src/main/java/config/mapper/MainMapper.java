d1945ecb283d2c9f3ab97c8c9efcf109e2154915
sinbum <sinbum@kakao.com>
2021-08-02 17:29:43 +0900

    게시판생성기능 추가, 현재세션아이디값의 채널목록을 구현하는 함수 추가.

--- src/main/java/config/mapper/MainMapper.java
+++ src/main/java/config/mapper/MainMapper.java
@@ -1,11 +1,26 @@
 package config.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Param;
+
+import vo.BoardChannelVO;
 
 public interface MainMapper {
 	public int loginCheck(@Param("id") String id,@Param("password") String password);
 
-	public int signIn(@Param("id")String id, @Param("password") String password,@Param("email") String email);
+	public int signIn(@Param("id") String id, @Param("password") String password,@Param("email") String email);
 
-	public int makeChannel();
+	public int makeChannel(@Param("channel_name") String channel_name,@Param("channel_info") String channel_info,@Param("channel_category") String channel_category,@Param("member_id_fk") String member_id_fk);
+	
+	public int makeBoardDo(
+			@Param("board_firstkeyword") String board_firstkeyword,
+			@Param("board_secondkeyword") String board_secondkeyword,
+			@Param("board_name") String board_name,
+			@Param("board_content") String board_content,
+			@Param("channel_num") int channel_num			
+			);
+	
+	public List<BoardChannelVO> getChannelList(@Param("memberId") String memberId);
+
 }
