00a19ab891374399c97f0e17a969985900ebac9c
sinbum <sinbum@kakao.com>
2021-08-02 17:28:23 +0900

    Mapper내 게시글의 총 갯수를 구하는 getMaxCount함수 추가

--- src/main/java/config/mapper/BoardMapper.java
+++ src/main/java/config/mapper/BoardMapper.java
@@ -2,10 +2,14 @@ package config.mapper;
 
 import java.util.List;
 
+
 import vo.BoardVO;
 
 public interface BoardMapper {
- public List <BoardVO> viewAll();
+ 
+public List <BoardVO> viewAll();
  public List <BoardVO> selectBoardList(int startIndex, int pageSize);
  public int selectBoardListCnt(BoardVO boardVO);
+public int getMaxCount();
+
 }
